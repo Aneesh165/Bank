ca93f1321c5421652eaf93a12aace5cd4be3f4a2
Aneesh165 <aneeshmp165@gmail.com>
2024-10-26 12:17:53 +0530

    26-10-24

--- Blog/src/components/Bank/BankHome.jsx
+++ Blog/src/components/Bank/BankHome.jsx
@@ -2,17 +2,42 @@ import { FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import Bank from '../../assets/Bank.png'
 import logo from '../../assets/logo.png'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const BankHome = () => {
-  const [users,setusers]=useState(18)
+  const [userCount, setUserCount] = useState(0); 
+
+  
+  useEffect(() => {
+    const fetchUserCount = async () => {
+      const token = localStorage.getItem('token');
+      try {
+        const response = await axios.get('http://localhost:8080/admin/home',{
+          headers: {
+              Authorization:  `Bearer ${token}` 
+          }
+      }); 
+        setUserCount(response.data.No_of_Users); 
+      } catch (error) {
+        console.error("Error fetching user count:", error); 
+      }
+    };
+
+    fetchUserCount(); 
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    
+};
+
   return (
     <div>
       <div className="flex justify-end pr-12 py-4 bg-sky-300 gap-8">
         
         <Link to='/viewusers'><h2 className="text-xl font-semibold">View Users</h2></Link>
         <button>
-          <Link to="/">
+          <Link to="/banklogin" onClick={handleLogout}>
             <FiLogOut size={28} />
           </Link>
         </button>
@@ -23,7 +48,7 @@ const BankHome = () => {
         </div>
         <div className="bg-sky-300 z-10 w-[300px] h-[250px] flex flex-col rounded-3xl justify-center items-center absolute top-[230px] left-[210px]">
           <h1 className="text-2xl">No of Users :</h1>
-          <h2 className="text-xl">{users}</h2>
+          <h2 className="text-xl">{userCount}</h2>
         </div>
         <div className="w-[300px] h-[250px] rounded-3xl absolute bottom-40 left-[340px]">
           <img src={logo} className="w-[100%] h-[100%] rounded-3xl" alt="" />
